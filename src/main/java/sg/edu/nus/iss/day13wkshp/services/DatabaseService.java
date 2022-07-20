648bb7b455190ccc6c00469464e21bf86cafd859
Muzakkir22 <muzakkir22@gmail.com>
2022-07-20 16:20:24 +0800

    workshop completed

--- src/main/java/sg/edu/nus/iss/day13wkshp/services/DatabaseService.java
+++ src/main/java/sg/edu/nus/iss/day13wkshp/services/DatabaseService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import sg.edu.nus.iss.day13wkshp.models.Contact;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.util.*;
 
@@ -45,18 +46,26 @@ public class DatabaseService {
         }
 	}
 
-	public void read(String fileId) {
+	public Contact read(String fileId) {
 
         try {
             
-            File f = new File(this.dataDir, fileId);
-            Scanner myReader = new Scanner(f);
-            while (myReader.hasNextLine()) {
-                System.out.println(myReader.nextLine());
-            }
-            myReader.close();
+            // File f = new File(this.dataDir, fileId);
+            // Scanner myReader = new Scanner(f);
+            // while (myReader.hasNextLine()) {
+            //    System.out.println(myReader.nextLine());
+            // }
+            // myReader.close();
 
             Contact contact = new Contact();
+
+            Path filePath = new File(this.dataDir, fileId).toPath();
+            Charset charset = Charset.forName("utf-8");
+            List<String> stringVal = Files.readAllLines(filePath, charset);
+
+            contact.setName(stringVal.get(1));
+            contact.setEmail(stringVal.get(2));
+            contact.setPhone(stringVal.get(3));
 
             return contact;
         } catch (IOException ex) {
